0886b5b116b35a8556c4175db5085dee733e3c91
AlexAltrex <alexaltrex@mail.ru>
2022-07-05 11:52:49 +0300

    1

--- pages/character/[id].tsx
+++ pages/character/[id].tsx
@@ -37,7 +37,7 @@ const CharacterItem: NextPage = () => {
     return (
         <MainLayout headTitle={`Rick and Morty | ${character?.name || "loading..."}`}>
 
-            {loading && <LinearPreloader/>}
+            {true && <LinearPreloader/>}
 
             <section className={style.character}>
                 <div className={style.inner}>
